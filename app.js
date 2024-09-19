c748f8278d39fc9178d0dadce6408997092630f0
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-19 21:15:31 +0500

    added files

---
+++ app.js
@@ -0,0 +1,164 @@
+// Testing Set of Conditions:
+
+
+// Logical Operators
+
+
+// 1.)  &&  ==>  AND Operator
+
+
+// var userWeight = +prompt("Enter your weight:");
+// var time = +prompt("Enter time:");
+
+// if ( userWeight < 120  &&  time < 6 ) {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// }
+
+
+// 2.)  ||  ==>  OR Operator
+
+
+// var userWeight = +prompt("Enter your weight:");
+// var time = +prompt("Enter time:");
+
+// if ( userWeight < 120  ||  time < 6 ) {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Better luck next time!");
+// }
+
+
+
+
+
+// var userWeight = +prompt("Enter your weight:");
+// var time = +prompt("Enter time:");
+// var gender = prompt("Enter your gender:");
+
+// if ( ( gender === "male"  || gender === "female" )  &&  
+//     userWeight < 120  &&  
+//     time < 6 ) {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Better luck next time!");
+// }
+
+
+
+// var resident = prompt("Enter resident:");
+
+
+// if ( resident === "U.S" ) {
+//     var userAge = +prompt("Enter your age:");
+//     if ( userAge > 65 || userAge < 21 ) {
+//         alert("allow!");
+//     }
+//     else {
+//         alert("age not matched!")
+//     }
+// }
+// else {
+//     alert("resident not matched!");
+// }
+
+
+// if ( ( userAge > 65 || userAge < 21 )  &&  resident === "U.S" ) {
+//     alert("allow!")
+// }
+// else {
+//     alert("not allow!")
+// }
+
+
+
+
+// var progLang = prompt("Enter your programming language:");
+
+// if ( progLang === "js" ) {
+//     var city = prompt("Enter your city:");
+//     if ( city === "karachi" ) {
+//         var exp = +prompt("Enter your experience:");
+//         if ( exp >= 2 ) {
+//             alert("You are hired!")
+//         }
+//         else {
+//             alert("experience lao..")
+//         }
+//     }
+//     else {
+//         alert("karachi ajao..")
+//     }
+// }
+// else {
+//     alert("js seekho!");
+// }
+
+
+
+// ARRAY:
+
+
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// var d = 40;
+// var e = 50;
+// var f = 60;
+// var g = 70;
+// var h = 80;
+
+
+
+// var numbers = [10,20,30,40,50,60,70,80];
+
+
+// var numbers = [10,"adil",30,true,50,undefined,70,80];
+
+
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers[4]);
+// console.log(numbers[5]);
+// console.log(numbers[6]);
+// console.log(numbers[7]);
+// console.log(numbers[8]);
+
+
+
+// console.log(numbers.length); 
+
+
+
+// var arr = [];
+
+// arr[2] = "adil";
+
+// console.log(arr[0]);
+
+
+
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+// var age = +prompt("Enter your age:");
+// var city = prompt("Enter your city:");
+
+
+
+// var arr = [prompt("Enter your first name:"),
+//     prompt("Enter your last name:"),
+//     +prompt("Enter your age:"),
+//     prompt("Enter your city:")
+// ];
+
+// console.log(`My name is ${arr[0]} ${arr[1]}. I am ${arr[2]} years old. I live in ${arr[3]}.`);
+
+
+
